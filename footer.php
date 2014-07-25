f0fa7cf567b64d5dd1b41b9ba3ce120f79d7ad60
root <root@arsalan.(none)>
2014-07-24 20:28:33 -0400

    seonc

---
+++ footer.php
@@ -0,0 +1,35 @@
+<!-- Footer -->
+<div id="footer">
+    <div class="container">
+     
+        <div class="row">
+            <div class="12u">
+                
+                <!-- Contact -->
+                    <section class="contact">
+                        <header>
+                            <h2>Contactenos:</h2>
+                        </header>
+                        <p>Telefono: </p>
+                        <h4>Oficina Administrativa</h4>
+                        <div>Telefono: 2265021 </div>
+                        <div>Dirección: Carrasquilla calle 85 al frente de la autoridad de aseo.</div>
+                       <!-- <img src="images/administracion.jpg" alt="" /> ->
+                        
+                    </section>
+                
+                <!-- Copyright -->
+                    <div class="copyright">
+                        <ul class="menu">
+                            <li>&copy; Todos los derechos reservador. Centro Educativo de Panama</li>
+                        </ul>
+                    </div>
+                
+            </div>
+        
+        </div>
+    </div>
+</div>
+
+</body>
+</html>
---
+++ menu.php
@@ -0,0 +1,52 @@
+<body class="homepage">
+
+<!-- Header -->
+    <div id="header">
+                
+        <!-- Inner -->
+            <div class="inner">
+                <header>
+                    <h1 class='header-title'>Centro Educativo de Panama</h1>
+                </header>
+                <footer>
+                    <a href="#banner" class="button circled scrolly">Play</a>
+                </footer>
+            </div>
+        
+        <!-- Nav -->
+            <nav id="nav">
+                <ul>
+                    <li><a href="left-sidebar.html">Misión</a></li>
+                    <li>
+                        <span>Historia</span>
+                        <ul>
+                            <li><a href="#">Biografia de los Musulmanes en Panama</a></li>
+                            <li><a href="#">America Latina</a></li>
+                            <li><a href="#">Posicion en Panama</a></li>
+                            <li><a href="#">Antigua Escuela - Aguadulce</a></li>
+
+                           <!--  <li>
+                                <span>Sistema Constructivo &hellip;</span>
+                                <ul>
+                                    <li><a href="#">Lorem ipsum dolor</a></li>
+                                    <li><a href="#">Phasellus consequat</a></li>
+                                    <li><a href="#">Magna phasellus</a></li>
+                                    <li><a href="#">Etiam dolore nisl</a></li>
+                                </ul>
+                            </li> -->
+                        </ul>
+                    </li>
+                    <li><a href="#">Visión</a></li>
+                    <li><a href="#">Mezquita</a></li>
+                    <li><a href="#">Escuela</a></li>
+                    <li><a href="#">Residencia</a></li>
+                    <li><a href="#">Infraestructura</a></li>
+                    <li><a href="#footer">Contactenos</a></li>
+
+
+
+
+                </ul>
+            </nav>
+
+    </div>
