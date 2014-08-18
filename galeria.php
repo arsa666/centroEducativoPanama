55eea98adb61b6085a4b7d9608063aa0860aff40
root <root@centroeducativopanama.(none)>
2014-08-18 18:56:49 -0400

    iamges and all changes

---
+++ galeria.php
@@ -0,0 +1,55 @@
+<?php require 'includes.php'?>
+<?php require 'menu.php'?>
+ 
+            
+<!-- Banner -->
+    <div class="container special">
+        <h2><strong>Galería</strong></h2>
+        <span class="byline">
+        <img class='galeria-img' src='images/const1.JPG' />
+        <img class='galeria-img' src='images/const2.JPG' />
+        <img class='galeria-img' src='images/const3.JPG' />
+        <img class='galeria-img' src='images/const4.JPG' />
+        <img class='galeria-img' src='images/const5.JPG' />
+        <img class='galeria-img' src='images/const6.JPG' />
+        <img class='galeria-img' src='images/const7.JPG' />
+        <img class='galeria-img' src='images/const8.JPG' />
+        <img class='galeria-img' src='images/const9.JPG' />
+        <img class='galeria-img' src='images/DSC01106.JPG' />
+        <img class='galeria-img' src='images/DSC01107.JPG' />
+        <img class='galeria-img' src='images/DSC01109.JPG' />
+        <img class='galeria-img' src='images/DSC01118.JPG' />
+        <img class='galeria-img' src='images/DSC01122.JPG' />
+        <img class='galeria-img' src='images/DSC01124.JPG' />
+        <img class='galeria-img' src='images/DSC01129.JPG' />
+        <img class='galeria-img' src='images/DSC01131.JPG' />
+        <img class='galeria-img' src='images/DSC01132.JPG' />
+        <img class='galeria-img' src='images/DSC01148.JPG' />
+        <img class='galeria-img' src='images/DSC01149.JPG' />
+        <img class='galeria-img' src='images/DSC01519.JPG' />
+        <img class='galeria-img' src='images/DSC01520.JPG' />
+        <img class='galeria-img' src='images/DSC01562.JPG' />
+        <img class='galeria-img' src='images/DSC01568.JPG' />
+        <img class='galeria-img' src='images/const10.jpg' />
+        <img class='galeria-img' src='images/const11.jpg' />
+        <img class='galeria-img' src='images/const12.jpg' />
+        <img class='galeria-img' src='images/const13.jpg' />
+        <img class='galeria-img' src='images/const14.jpg' />
+        <img class='galeria-img' src='images/const15.jpg' />
+        <img class='galeria-img' src='images/const16.jpg' />
+        <img class='galeria-img' src='images/const17.jpg' />
+        <img class='galeria-img' src='images/const18.jpg' />
+        <img class='galeria-img' src='images/const19.jpg' />
+        <img class='galeria-img' src='images/const20.jpg' />
+        <img class='galeria-img' src='images/const21.jpg' />
+        <img class='galeria-img' src='images/const22.jpg' />
+        <img class='galeria-img' src='images/const23.jpg' />
+
+
+
+        </span>
+    </div>        
+
+<?php require 'footer.php'?>
+
+       
